1d2143860001c340dab598c59c25bed94e4d682b
Mahammad V. Mustafayev <mahammadvm@code.local>
2023-06-09 12:39:06 +0400

    added

---
+++ SQLExternalFunctions/SQLExternalFunctions/SQLExternalFunctions .cs
@@ -0,0 +1,17 @@
+﻿
+
+using Microsoft.SqlServer.Server;
+using System.Linq;
+
+public static class SQLExternalFunctions
+{
+    [SqlFunction]
+	public static int GetCharacterOccurence(string str, char chr)
+	{
+		if (string.IsNullOrWhiteSpace(str))
+			return 0;
+
+		return str.Where(f => f == chr).Count();
+	}
+
+}
